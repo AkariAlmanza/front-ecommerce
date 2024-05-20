18ed4cd11f801929d5de275a379366875f469210
AkariAlmanza <ag.almanzaacosta@ugto.mx>
2024-05-19 23:29:47 -0600

    update front

---
+++ components/InicioPag/InicioPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-title>
+      inico
+    </v-card-title>
+    <v-card-actions>
+      Botones
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
