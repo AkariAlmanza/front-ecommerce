63f9062cca8c5060678e3946d2e09e62b33f2f7a
AkariAlmanza <ag.almanzaacosta@ugto.mx>
2024-05-20 09:44:41 -0600

    Intentodefront

---
+++ components/piePag.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer app>
+    <v-col class="text-center">
+      © 2024 My E-commerce
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'PagFondo'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,45 @@
 <template>
-  <velasIniVue />
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Welcome to My E-commerce</h1>
+        <v-carousel>
+          <v-carousel-item v-for="i in 4" :key="i"></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="product in products" :key="product.id" cols="12" sm="6" md="4">
+        <v-card>
+          <v-card-title>{{ product.name }}</v-card-title>
+          <v-card-subtitle>{{ product.price }}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn text @click="addToCart(product)">
+              Add to Cart
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import velasIniVue from '@/components/iniciopage/velas-ini.vue'
-
 export default {
-  name: 'IndexPage',
-  components: {
-    velasIniVue
+  data () {
+    return {
+      products: [
+        { id: 1, name: 'Product 1', price: '$10.00', image: 'https://picsum.photos/200/300' },
+        { id: 2, name: 'Product 2', price: '$20.00', image: 'https://picsum.photos/200/300' },
+        // Add more products as needed
+      ]
+    }
   },
-  layout: 'iniciopage'
+  methods: {
+    addToCart (product) {
+      // Implement add to cart functionality
+      console.log(`${product.name} added to cart`)
+    }
+  }
 }
 </script>
